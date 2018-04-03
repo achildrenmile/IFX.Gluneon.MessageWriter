09785388c116f88390c58887169a89fdb02a6126
achildrenmile <michael@strali.solutions>
2018-04-03 13:55:04 +0200

    Initial commit

---
+++ IFX.Gluneon.MessageWriter/Message.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using ProtoBuf;
+
+namespace IFX.Gluneon.MessageWriter
+{
+
+    [ProtoContract]
+    public class Message
+    {
+        public Message() {
+            
+        }
+        public Guid Id { get; set; }
+        [ProtoMember(1)]
+        public string Channel { get; set; }
+        [ProtoMember(2)]
+        public string Publisher { get; set; }
+        [ProtoMember(3)]
+        public string Protocol { get; set; }
+        [ProtoMember(4)]
+        public string Name { get; set; }
+        [ProtoMember(5)]
+        public string Unit { get; set; }
+        [ProtoMember(6)]
+        public double Value { get; set; }
+        [ProtoMember(7)]
+        public string StringValue { get; set; }
+        [ProtoMember(8)]
+        public bool BoolValue { get; set; }
+        [ProtoMember(9)]
+        public string DataValue { get; set; }
+        [ProtoMember(10)]
+        public double ValueSum { get; set; }
+        [ProtoMember(11)]
+        public double Time { get; set; }
+        [ProtoMember(12)]
+        public double UpdateTime { get; set; }
+        [ProtoMember(13)]
+        public string Link { get; set; }
+    }
+}
---
+++ IFX.Gluneon.MessageWriter/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using NATS.Client;
+
+namespace IFX.Gluneon.MessageWriter
+{
+  
+    public class Program
+    {
+        public static IConfiguration Configuration { get; set; }
+        public const string MessageTopic="normalized";
+
+        public static void Main(string[] args)
+        {
+
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+
+            try
+            {
+                ConnectionFactory cf = new ConnectionFactory();
+                IConnection c = cf.CreateConnection($"{Configuration["NATS"]}");
+           
+
+                try 
+                {
+                    MongoDBContext.ConnectionString = $"{Configuration["ConnectionString"]}";
+                    MongoDBContext.DatabaseName = $"{Configuration["Database"]}";
+                    MongoDBContext.IsSSL = $"{Configuration["UseTLSforMongo"]}"=="TRUE"?true:false;
+                    MongoDBContext context = new MongoDBContext();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(string.Format("{0}\tException on configuration of the MongoDBContext: {1}", DateTime.Now.ToUniversalTime(), e.Message));
+                }
+
+                EventHandler<MsgHandlerEventArgs> WriteMessageHandler = (sender, argsMsg) =>
+                {
+                    try
+                    {
+                        var msg = ProtoBuf.Serializer.Deserialize<Message>(new System.IO.MemoryStream(argsMsg.Message.Data));//argsMsg.Message.Data);
+
+    #if DEBUG
+                        Console.WriteLine(string.Format(@"Channel: {0}\n
+                                                    Publisher: {1}\n
+                                                    Protocol: {2}
+                                                    Name: {3}\n
+                                                    Unit: {4}\n
+                                                    Value: {5}\n
+                                                    StringValue: {6}\n
+                                                    BoolValue: {7}\n
+                                                    DataValue: {8}\n
+                                                    ValueSum: {9}\n
+                                                    Time: {10}\n
+                                                    UpdateTime: {11}\n
+                                                    Link: {12}\n",
+                                                        msg.Channel,
+                                                        msg.Publisher,
+                                                        msg.Protocol,
+                                                        msg.Name,
+                                                        msg.Unit,
+                                                        msg.Value,
+                                                        msg.StringValue,
+                                                        msg.BoolValue,
+                                                        msg.DataValue,
+                                                        msg.ValueSum,
+                                                        msg.Time,
+                                                        msg.UpdateTime,
+                                                        msg.Link)
+                                         );
+    #endif               
+                        MongoDBContext dbContext = new MongoDBContext();
+                        msg.Id = Guid.NewGuid();
+                        dbContext.Messages.InsertOne(msg);
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine(string.Format("{0}\tException in Eventhanlder occured: {1}", DateTime.Now.ToUniversalTime(), e.Message));
+                    }
+
+                };
+
+                    IAsyncSubscription s2 = c.SubscribeAsync(MessageTopic, WriteMessageHandler);
+                }
+            catch(Exception e)
+            {
+                Console.WriteLine(string.Format("{0}\tNATS exception occured: {1}", DateTime.Now.ToUniversalTime(), e.Message));
+            }
+           
+        }
+    }
+}
